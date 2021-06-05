5f061cbde9d78c359c20e1571bd659f79fb61c0b
Chaitanya Singh Bisht <csbwork18@gmail.com>
2021-06-05 19:46:15 +0530

    Initial commit

---
+++ api.py
@@ -0,0 +1,43 @@
+from flask import Flask, request
+from app import sum, difference, multiply, division
+
+app = Flask(__name__)
+
+@app.route("/sum", methods=['POST', 'GET'])
+def addition():
+    if request.method == 'POST':
+        a = request.args.get('first')
+        b = request.args.get('second')
+        return str(sum(int(a), int(b)))
+
+@app.route("/difference", methods=['POST', 'GET'])
+def subtraction():
+    if request.method == 'POST':
+        a = request.args.get('first')
+        b = request.args.get('second')
+        return str(difference(int(a), int(b)))
+
+@app.route("/product", methods=['POST', 'GET'])
+def multiplication():
+    if request.method == 'POST':
+        a = request.args.get('first')
+        b = request.args.get('second')
+        return str(multiply(int(a), int(b)))
+
+@app.route("/divide", methods=['POST', 'GET'])
+def division():
+    if request.method == 'POST':
+        a = request.args.get('first')
+        b = request.args.get('second')
+        return str(division(int(a), int(b)))
+
+@app.route("/")
+def home():
+    return """For Addition go to \sum.\n
+    For Subtraction go to \diff.\n
+    For Multiplication go to \product.\n
+    For Division go to \divide."""
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
---
+++ app.py
@@ -0,0 +1,12 @@
+def sum(a, b):
+    return a+b
+
+def difference(a, b):
+    return a-b
+
+def multiply(a, b):
+    return a*b
+
+def division(a, b):
+    return a/b
+
